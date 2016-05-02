d2d84aa323a625fd2a66d7612825798e61af3318
hitode221 <marina.dr98@mail.ru>
2016-05-02 23:39:10 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -5,7 +5,7 @@
 
 SCENARIO("Init", "[Tree init]"){
   BinarySearchTree <int> tree(5);
-  BinarySearchTree <int> empty_tree();
+  BinarySearchTree <int> empty_tree;
   REQUIRE(tree.get_root()->data == 5);
   REQUIRE(empty_tree.get_root() == nullptr);
 }
